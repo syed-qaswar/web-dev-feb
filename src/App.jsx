d911f0301327fcb36f1aebc1cc256b65ebfab170
Syed Qaswar <qaswarbukhari5@gmail.com>
2026-06-06 14:51:33 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Welcome to our New React Project</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Deployed on Vercel
           </p>
         </div>
         <button
